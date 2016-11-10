a9ee3f48682e3b95e4f6809fd12fa537dcbb491b
richard-burak <rickyburak@gmail.com>
2016-11-10 15:04:47 -0500

    Test comment 3

--- springapplication/src/main/java/brave8/spring/HomeActivity.java
+++ springapplication/src/main/java/brave8/spring/HomeActivity.java
@@ -13,7 +13,7 @@ import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-//RICKy
+//RICKy is fone
 public class HomeActivity extends AppCompatActivity {
 
     @Override
