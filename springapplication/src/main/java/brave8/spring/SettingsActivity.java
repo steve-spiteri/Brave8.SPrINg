0fc5a7e2f36816985c6a3723eb2993d5fe0f5fbc
steve-spiteri <steve_spiteri@hotmail.com>
2016-11-10 15:49:21 -0500

    Testing

--- springapplication/src/main/java/brave8/spring/SettingsActivity.java
+++ springapplication/src/main/java/brave8/spring/SettingsActivity.java
@@ -11,7 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
-
+//hello
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
