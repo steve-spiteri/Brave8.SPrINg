2ea5e6a7d2844c92a3d97a6790bbc2df513c751b
richard-burak <rickyburak@gmail.com>
2016-11-11 18:00:54 -0500

    Added Solar Object

---
+++ springapplication/src/main/java/brave8/spring/Solar.java
@@ -0,0 +1,82 @@
+package brave8.spring;
+
+import java.sql.Date;
+
+/**
+ * Created by Ricky on 2016-11-11.
+ */
+
+public class Solar {
+    int idData;
+    int idLogin;
+    double power;
+    double temperature;
+    double light;
+    double barometric;
+    double humidity;
+    Date date;
+
+    public int getIdData() {
+        return idData;
+    }
+
+    public void setIdData(int idData) {
+        this.idData = idData;
+    }
+
+    public int getIdLogin() {
+        return idLogin;
+    }
+
+    public void setIdLogin(int idLogin) {
+        this.idLogin = idLogin;
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    public void setPower(double power) {
+        this.power = power;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getLight() {
+        return light;
+    }
+
+    public void setLight(double light) {
+        this.light = light;
+    }
+
+    public double getBarometric() {
+        return barometric;
+    }
+
+    public void setBarometric(double barometric) {
+        this.barometric = barometric;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(double humidity) {
+        this.humidity = humidity;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
---
+++ springapplication/src/main/java/brave8/spring/SolarDataSource.java
@@ -0,0 +1,8 @@
+package brave8.spring;
+
+/**
+ * Created by Ricky on 2016-11-11.
+ */
+
+public class SolarDataSource {
+}
