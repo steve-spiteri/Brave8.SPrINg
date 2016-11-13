020f6b230f2647cf478260035404ed5a140bb93f
richard-burak <rickyburak@gmail.com>
2016-11-13 18:04:07 -0500

    Created Module for Database Connection Testing

---
+++ databasetest/src/main/java/com/example/ricky/databasetest/Config.java
@@ -0,0 +1,12 @@
+package com.example.ricky.databasetest;
+
+/**
+ * Created by Ricky on 2016-11-13.
+ */
+
+public class Config {
+    public static final String DATA_URL = "http://192.168.1.109/spring/test2.php?id=";
+    public static final String KEY_ID_DATA = "id_data";
+    public static final String KEY_POWER = "power";
+    public static final String JSON_ARRAY = "result";
+}
