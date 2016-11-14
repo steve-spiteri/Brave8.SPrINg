a0c6fed380f60626b8bfb27d92fbd7c44169b661
steve-spiteri <steve_spiteri@hotmail.com>
2016-11-14 10:45:04 -0500

    Edited labels to graph

--- springapplication/src/main/java/brave8/spring/PageFragment.java
+++ springapplication/src/main/java/brave8/spring/PageFragment.java
@@ -108,11 +108,12 @@ public class PageFragment extends Fragment {
             graph = (GraphView) view.findViewById(R.id.graph);
 
             //----------
-            GridLabelRenderer gridLabelRenderer = graph.getGridLabelRenderer();
+            final GridLabelRenderer gridLabelRenderer = graph.getGridLabelRenderer();
             if (Build.VERSION.SDK_INT >= 23) {
                 gridLabelRenderer.setGridColor(ContextCompat.getColor(getContext(), R.color.black));
                 gridLabelRenderer.setHorizontalLabelsColor(ContextCompat.getColor(getContext(), R.color.black));
                 gridLabelRenderer.setVerticalLabelsColor(ContextCompat.getColor(getContext(), R.color.black));
+                //gridLabelRenderer.setHorizontalAxisTitle("test");
             } else {
                 gridLabelRenderer.setGridColor(getResources().getColor(R.color.black));
                 gridLabelRenderer.setHorizontalLabelsColor(getResources().getColor(R.color.black));
@@ -136,38 +137,56 @@ public class PageFragment extends Fragment {
                             values[i] = new DataPoint(i,num/4); //average of the four numbers
                             num=0.0; //reset num so it can be used again
                         }
-                        series = new LineGraphSeries<>(values);
-                        graph.addSeries(series);
+                        series = new LineGraphSeries<>(values); //add values to series
+                        graph.addSeries(series); //add series to graph
+                        graph.getViewport().setXAxisBoundsManual(true);
+                        graph.getViewport().setMaxX(24); //so you can see last label
+                        gridLabelRenderer.setVerticalAxisTitle("Voltage"); //set vertical axis title
+                        gridLabelRenderer.setHorizontalAxisTitle("Hour"); //set horizontal axis title
+                        gridLabelRenderer.setNumHorizontalLabels(12); //set number of horizontal labels
+                        graph.getGridLabelRenderer().reloadStyles(); //reload style
                     }
                     else if(pos==1){
-                        graph.removeAllSeries();
-                        values = new DataPoint[7];
-                        for (int i =0,k=power.length-672;i<values.length;i++,k+=96)
+                        graph.removeAllSeries(); //clear last graph
+                        values = new DataPoint[7]; //7 days
+                        for (int i =0,k=power.length-672;i<values.length;i++,k+=96) //7 entries, k=end of array - (24*4)*7
                         {
-                            for(int x=0;x<96;x++)
+                            for(int x=0;x<96;x++) //combine 96 15 min entries for 1 day
                             {
-                                num+=power[k+x];
+                                num+=power[k+x]; //add 96 entries into one variable
                             }
-                            values[i] = new DataPoint(i,num/96);
-                            num=0.0;
+                            values[i] = new DataPoint(i,num/96); //average the 96 numbers
+                            num=0.0; //reset num so it can be used again
                         }
                         series = new LineGraphSeries<>(values);
                         graph.addSeries(series);
+                        graph.getViewport().setXAxisBoundsManual(true);
+                        graph.getViewport().setMaxX(7); //so you can see last label
+                        gridLabelRenderer.setVerticalAxisTitle("Voltage");
+                        gridLabelRenderer.setHorizontalAxisTitle("Day");
+                        gridLabelRenderer.setNumHorizontalLabels(7);
+                        graph.getGridLabelRenderer().reloadStyles();
                     }
                     else if(pos==2){
-                        graph.removeAllSeries();
-                        values = new DataPoint[4];
-                        for (int i =0,k=power.length-2688;i<values.length;i++,k+=672)
+                        graph.removeAllSeries(); //clear last graph
+                        values = new DataPoint[4]; //4 weeks
+                        for (int i =0,k=power.length-2688;i<values.length;i++,k+=672) //4 entries, k=end of array = ((24*4)*7)*4
                         {
-                            for(int x=0;x<672;x++)
+                            for(int x=0;x<672;x++) //combine 672 15 min entries int one vatiable
                             {
-                                num+=power[k+x];
+                                num+=power[k+x]; //add 672 entries into one variable
                             }
-                            values[i] = new DataPoint(i,num/672);
-                            num=0.0;
+                            values[i] = new DataPoint(i,num/672); //average the 672 numbers
+                            num=0.0; //reset num so it can be used again
                         }
                         series = new LineGraphSeries<>(values);
                         graph.addSeries(series);
+                        graph.getViewport().setXAxisBoundsManual(true);
+                        graph.getViewport().setMaxX(4); //so you can see last label
+                        gridLabelRenderer.setVerticalAxisTitle("Voltage");
+                        gridLabelRenderer.setHorizontalAxisTitle("Week");
+                        gridLabelRenderer.setNumHorizontalLabels(4);
+                        graph.getGridLabelRenderer().reloadStyles();
                     }
                 }
                 public void onNothingSelected(AdapterView<?> parent)
