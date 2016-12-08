d180c032b548600419e10d03bd6dbfc5a6a24e4f
steve-spiteri <steve_spiteri@hotmail.com>
2016-12-07 20:00:32 -0500

    Added the ability to check internet connection and database connection

--- springapplication/src/main/java/brave8/spring/LoginActivity.java
+++ springapplication/src/main/java/brave8/spring/LoginActivity.java
@@ -8,25 +8,40 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button signIn;
-    EditText dbUser;
-    EditText dbPass;
+    EditText dbUser, dbPass;
+    LinearLayout status_layout, login_layout;
+    TextView status;
     public static final String MY_PREFS_NAME = "AppUsers";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        new CheckStatus().execute();
 
         //if device is xlarge(tablet) make the orientation always landscape
         int screenSize = getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK;
@@ -91,5 +106,75 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private class CheckStatus extends AsyncTask<Void, Void, Integer> {
+
+        @Override
+        protected Integer doInBackground(Void... v) {
+            boolean internetConnection = false;
+            boolean databaseConnection = false;
+            try {
+                URL url = new URL("https://www.google.ca/");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setConnectTimeout(10000);
+                connection.connect();
+                internetConnection = connection.getResponseCode() == 200;
+            }
+            catch (IOException e)
+            {
+            }
+
+            HttpHandler sh = new HttpHandler();
+            String jsonStr = sh.makeServiceCall("http://springdb.eu5.org/spring/check_connection.php");;
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+                    JSONArray result = jsonObj.getJSONArray("result");
+                    String db_conn = result.getJSONObject(0).getString("db_conn");
+                    if(db_conn.equals("success"))
+                    {
+                        databaseConnection = true;
+                    }
+                }
+                catch (JSONException e)
+                {
+                }
+            }
+
+            if (internetConnection&&databaseConnection)
+            {
+                return 1;
+            }
+            else if (!internetConnection)
+            {
+                return -1;
+            }
+            else if (internetConnection&&!databaseConnection)
+            {
+                return -2;
+            }
+            return -3;
+        }
+
+        protected void onPostExecute(Integer result) {
+            status_layout = (LinearLayout) findViewById(R.id.status_layout);
+            login_layout = (LinearLayout) findViewById(R.id.login_layout);
+            status = (TextView) findViewById(R.id.status);
+            if (result == 1)
+            {
+                status_layout.setVisibility(View.GONE);
+                login_layout.setVisibility(View.VISIBLE);
+            }
+            else if (result == -1)
+            {
+                status.setText("no internet");///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            }
+            else if (result == -2)
+            {
+                status.setText("no database connection");//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            }
+
+        }
     }
 }
