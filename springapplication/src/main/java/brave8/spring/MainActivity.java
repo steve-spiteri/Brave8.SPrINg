62bc2d5183b7977070a9a7a11c9d77c7ad4bacae
richard-burak <rickyburak@gmail.com>
2016-12-08 16:15:40 -0500

    Merge branches 'Branch.Richard' and 'master' of https://github.com/steve-spiteri/Brave8.SPrINg into Branch.Richard
    Conflicts:
    	springapplication/src/main/java/brave8/spring/LoginActivity.java
    	springapplication/src/main/java/brave8/spring/MainActivity.java
    	springapplication/src/main/java/brave8/spring/PageFragment.java
    	springapplication/src/main/java/brave8/spring/SettingsActivity.java

--- springapplication/src/main/java/brave8/spring/MainActivity.java
+++ springapplication/src/main/java/brave8/spring/MainActivity.java
@@ -101,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId())
         {
-            case R.id.search:
+            case R.id.settings:
                 Intent intent = new Intent(this, SettingsActivity.class);
                 startActivity(intent);
                 break;
