774b881a6693336f6769a7a773cfd25dfee65ca6
steve-spiteri <steve_spiteri@hotmail.com>
2016-12-09 00:00:53 -0500

    Added JUnit

---
+++ springapplication/src/test/java/brave8/spring/HttpHandlerTest.java
@@ -0,0 +1,28 @@
+/**
+ * Brave 8, Project SPrINg
+ */
+package brave8.spring;
+
+import org.junit.Test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HttpHandlerTest {
+
+    private HttpHandler mHttpHandler;
+
+    @Before
+    public void setUp() throws Exception {
+        mHttpHandler = new HttpHandler();
+    }
+
+    @Test
+    public void makeServiceCall() throws Exception {
+        String url = "http://springdb.eu5.org/spring/login.php?user=invalid&pass=invalid";
+        assertEquals("  {\"result\":[{\"id_login\":\"-1\"}]} \n",mHttpHandler.makeServiceCall(url));
+    }
+
+}
---
+++ springapplication/src/test/java/brave8/spring/LoginActivityTest.java
@@ -0,0 +1,29 @@
+/**
+ * Brave 8, Project SPrINg
+ */
+package brave8.spring;
+
+import org.junit.Test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+public class LoginActivityTest {
+
+    private LoginActivity mLoginActivity;
+
+    @Before
+    public void setUp() throws Exception {
+        mLoginActivity = new LoginActivity();
+    }
+
+    @Test
+    public void isEmpty() throws Exception {
+        String string = "test";
+        assertFalse(mLoginActivity.isEmpty(string));
+    }
+
+}
